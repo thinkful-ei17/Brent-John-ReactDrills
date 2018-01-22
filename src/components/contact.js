bcbf2778f2072c8acfab9626492d61bdbe426860
Brent Guistwite <BrentGuistwite@gmail.com>
2018-01-22 14:41:16 -0500

    finished drill

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,14 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+
+export default function Contact(props) {
+  console.log(props);
+  return (
+    <div className="contact">
+      <img className="contact-photo" src={props.photo} alt="contact icon" />
+      <h2 className="contact-name">{props.name}</h2>
+      <p className="contact-address">{props.address}</p>
+    </div>
+  );
+}
