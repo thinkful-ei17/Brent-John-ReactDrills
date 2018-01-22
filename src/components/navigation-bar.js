a227b3a1200469cfd8aea204b30ac5fe21a2e441
Brent Guistwite <BrentGuistwite@gmail.com>
2018-01-22 13:37:36 -0500

    finished first drill

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -4,3 +4,23 @@ import './navigation-bar.css';
 
 // The NavigationBar component goes here.  It should be the default export.
 
+export default function NavigationBar(props) {
+  const newLinks = props.links.map(link =>
+    (<li>
+      <a href={link.href}>{link.text}</a>
+    </li>
+    ));
+  console.log("hello world", newLinks);
+  return (
+    <div className="navigation-bar">
+      <h1>{props.title}</h1>
+      <nav className="navigation-bar-nav">
+        <ul>
+          {newLinks}
+        </ul>
+      </nav>
+    </div>
+  );
+
+
+}
